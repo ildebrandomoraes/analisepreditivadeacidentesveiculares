b71bb963abfafec4de7978b07167337ad3a798da
ildebrandomoraes <brando.moraes@gmail.com>
2025-02-24 03:08:25 -0300

    foi implementado uma janela de busca por rodovia e tambem os dados do csv foi filtrado pra somente o estado de sâo paulo

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ def load_data():
     data_path = os.path.join(os.path.dirname(__file__), 'accidents_2017_to_2023_portugues.csv')
     df = pd.read_csv(data_path)
     df['date'] = pd.to_datetime(df['date'])
+    
+
     return df
 
 # Rota principal que renderiza o mapa
@@ -35,24 +37,33 @@ def index():
 
     return render_template('index.html', map_html=map_html)
 
-# Rota para filtrar por data
+# Rota para filtrar por data e BR
 @app.route('/filter', methods=['POST'])
 def filter_data():
     start_date = request.form.get('start_date')
     end_date = request.form.get('end_date')
-    
-    # Carregar dados e filtrar por data
+    br_filter = request.form.get('br_filter')  # Novo campo para filtrar por BR
+
+    # Carregar dados
     df = load_data()
-    start_date = datetime.strptime(start_date, '%Y-%m-%d')
-    end_date = datetime.strptime(end_date, '%Y-%m-%d')
-    filtered_df = df[(df['date'] >= start_date) & (df['date'] <= end_date)]
+
+    # Aplicar filtro de data
+    if start_date and end_date:
+        start_date = datetime.strptime(start_date, '%Y-%m-%d')
+        end_date = datetime.strptime(end_date, '%Y-%m-%d')
+        df = df[(df['date'] >= start_date) & (df['date'] <= end_date)]
+
+    # Aplicar filtro de BR (se fornecido)
+    if br_filter:
+        br_filter = float(br_filter)  # Converter para float
+        df = df[df['br'] == br_filter]
 
     # Criar o mapa com os dados filtrados
-    m = folium.Map(location=[filtered_df['latitude'].mean(), filtered_df['longitude'].mean()], zoom_start=12)
+    m = folium.Map(location=[df['latitude'].mean(), df['longitude'].mean()], zoom_start=12)
     marker_cluster = MarkerCluster().add_to(m)
 
     # Adicionar marcadores no mapa
-    for _, row in filtered_df.iterrows():
+    for _, row in df.iterrows():
         folium.Marker(
             location=[row['latitude'], row['longitude']],
             popup=f"Data: {row['date'].strftime('%Y-%m-%d')}").add_to(marker_cluster)
