80811acef21725201a9be18f7768790c30efb4eb
ildebrandomoraes <brando.moraes@gmail.com>
2025-02-28 02:13:15 -0300

    projeto ganhou uma cara mais colorida com botões coloridos mais apresentavel

---
+++ templates/script.js
@@ -0,0 +1,15 @@
+document.getElementById('mapaBtn').addEventListener('click', function() {
+    alert('Você será redirecionado para o Mapa!');
+});
+
+document.getElementById('graficoBtn').addEventListener('click', function() {
+    alert('Você será redirecionado para o Gráfico!');
+});
+
+document.getElementById('regiõesbtn').addEventListener('click', function() {
+    alert('Você será redirecionado para o Mapa Choropleth!');
+});
+
+document.getElementById('mapacalorbtnBtn').addEventListener('click', function() {
+    alert('Você será redirecionado para o Mapa de Calor!');
+});
